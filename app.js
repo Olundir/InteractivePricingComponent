6f0240cd8e1caaf21e0bb19e2cb520f2ab46e828
Olundir <krzysztof.jezak.contact@gmail.com>
2021-07-13 12:39:34 +0200

    layout done, basic js done. needs the range slider styling

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const slider = document.querySelector("#rangeViews");
+const pageViews = document.querySelector("#viewsN");
+const amount = document.querySelector("#amount");
 
-slider.addEventListener("change", () => {
-  console.log("clicked");
-  console.log(slider.value);
+slider.addEventListener("input", () => {
+  switch (slider.value) {
+    case "0":
+      pageViews.innerText = "10K";
+      amount.innerText = "$4.00";
+      break;
+    case "1":
+      pageViews.innerText = "25K";
+      amount.innerText = "$8.00";
+      break;
+    case "2":
+      pageViews.innerText = "100K";
+      amount.innerText = "$16.00";
+      break;
+    case "3":
+      pageViews.innerText = "500K";
+      amount.innerText = "$24.00";
+      break;
+    case "4":
+      pageViews.innerText = "1M";
+      amount.innerText = "$36.00";
+      break;
+  }
 });
